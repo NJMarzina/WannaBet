91695838a0976aa06c6d20899c666bfc2def0243
Nathan Marzina <46549002+NJMarzina@users.noreply.github.com>
2024-12-16 18:39:04 -0500

    Add project files.

---
+++ WannaBetClassLibrary/Wager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WannaBetClassLibrary
+{
+
+    public class Wager
+    {
+        String description;
+        double senderAmount;
+        double senderMultiplier;
+
+        public String Description
+        {
+            get
+            {
+                return this.description;
+            }
+            set
+            {
+                this.description = value;
+            }
+        }
+
+        public double SenderAmount
+        {
+            get
+            {
+                return this.senderAmount;
+            }
+            set
+            {
+                this.senderAmount = value;
+            }
+        }
+
+        public double SenderMultiplier
+        {
+            get
+            {
+                return this.senderMultiplier;
+            }
+            set
+            {
+                this.senderMultiplier = value;
+            }
+        }
+    }
+}
